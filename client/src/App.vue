20f2f46c7c4bde05d8e95fcfda61896b42accbae
dm1trycril <akrylov008@gmail.com>
2022-05-23 19:07:49 +0300

    Added global registration of UI components, added new components
    Added navbar and button components

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="app">
+        <nav-bar-ui></nav-bar-ui>
+    </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style>
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
