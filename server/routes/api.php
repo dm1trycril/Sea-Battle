2fbe1ceb8204c82bccfbd27fdd6ee49e36bdd1b9
dm1trycril <akrylov008@gmail.com>
2022-06-03 22:58:53 +0300

    Dev proxy configured

--- server/routes/api.php
+++ server/routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api/ping', function (Request $request) {
+Route::get('/ping', function (Request $request) {
     // Log::info("ping pong");
-    return 'pong';
+    return response()->json('pong');
 });
